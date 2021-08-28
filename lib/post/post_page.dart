5c27b773326e660308cddb3dd1c882b2dc32e0ed
corp51 <sysdjp@gmail.com>
2021-08-28 16:25:17 +0900

    commit stateless(送信ボタンを追加

--- lib/post/post_page.dart
+++ lib/post/post_page.dart
@@ -8,28 +8,43 @@ class PostPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      TextField(
-        controller: titleTextController,
-        maxLength: 64,
-        decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: "タイトル",
-            hintText: "タイトルを入力して下さい"),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(children: [
+          TextField(
+            controller: titleTextController,
+            maxLength: 64,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "タイトル",
+                hintText: "タイトルを入力して下さい"),
+          ),
+          TextField(
+            controller: contentTextController,
+            maxLines: null,
+            maxLength: 250,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: '投稿内容',
+              hintText: '投稿内容を入力して下さい。改行で複数行入力可',
+              alignLabelWithHint: true,
+              filled: true,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('送信'),
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+            )),
+          ),
+        ]),
       ),
-      TextField(
-        controller: contentTextController,
-        maxLines: null,
-        maxLength: 250,
-        keyboardType: TextInputType.multiline,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: '投稿内容',
-          hintText: '投稿内容を入力して下さい。改行で複数行入力可',
-          alignLabelWithHint: true,
-          filled: true,
-        ),
-      )
-    ]);
+    );
   }
 }
